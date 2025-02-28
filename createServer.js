330fd2044c888c47d76ff96b9bd07e8ab97bcda4
ghulammustafaghori <ghulammustafaghori>
2025-02-28 17:25:22 +0500

    createServer.js

---
+++ createServer.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+// http.createServer((request,response)=>{
+//     response.writeHead(200,{ 'Content-Type': 'text/html'})
+//     response.write("<h2>Hello From Node JS</h2>")
+//     response.end();
+// }).listen(1000)
+
+//function as a parameter
+const sayHello=(request,response)=>{
+    response.write("Hello from sayHello function")
+    response.end()
+}
+http.createServer(sayHello).listen(1000)
