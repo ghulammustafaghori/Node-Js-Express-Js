8ad8eacf65578aa06c4c3ede13046eb53e75bc54
ghulammustafaghori <ghulammustafaghori>
2025-03-01 23:07:03 +0500

    asynchronous.js (most important)

--- asynchronous.js
+++ asynchronous.js
@@ -1,7 +1,45 @@
+// let firstName="Ghulam";
+// let lastName="";
+// // creating promise 
+// const setLastName= new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         lastName="Mustafa"
+//         resolve()
+//     },2000)
+// })
+// // handle promise 
+// setLastName.then(()=>{
+//     console.log(firstName+" "+lastName);
+// })
+// console.log("hello");
+
+
+
+
+// method 2 
+// let firstName="Ghulam";
+// let lastName="";
+// // creating promise 
+const setLastName= new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        
+        resolve("Mustafa")
+    },2000)
+})
+// // handle promise 
+setLastName.then((data)=>{
+    lastName=data
+})
+// console.log("hello");
+
+
+
+//async await
 let firstName="Ghulam";
 let lastName="";
-// console.log("a")
-setTimeout(()=>{
-lastName="Mustafa"
-},2000)
-console.log(firstName+""+lastName);
+const displayFullName=async()=>{
+    lastName=await setLastName;
+    console.log(`${firstName} ${lastName}`)
+}
+displayFullName();
+
